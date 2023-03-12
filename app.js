9d94d431df81b0bf237fe8da7f74b0e08242e153
Dimetrios <105342625+DumitruCiobanu@users.noreply.github.com>
2023-03-12 23:12:16 +0200

    Updated app.js
    Steps 2-4 (depend on each other in my code)

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const buttons = document.getElementsByTagName('button');
 
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("mousedown", function () {
+    this.classList.add("pressed");
+
+  setTimeout(() => {
+      this.classList.remove("pressed");    
+}, 3000); 
+  }); 
+}
+
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("mousedown", function () {
+  this.classList.add("released");
+
+  setTimeout(() => {
+    this.classList.remove("released");
+    }, 60000); 
+}); 
+}
